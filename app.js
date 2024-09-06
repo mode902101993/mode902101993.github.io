2a522bf841c055f8c2777d695fa5cc42c3ae48cc
mode902101993 <mode90210@hotmail.co.uk>
2024-09-06 17:07:39 +0100

    Initial commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll("img");
 
     for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
+        fetch("DALL·E 2024-09-06 13.23.18 - A futuristic skyscraper made of glass and steel with organic flowing lines and rooftop gardens, surrounded by smaller modern buildings. The scene is s.webp")
             .then(response => response.json ())
             .then(data => {
                 image.src = data.message
